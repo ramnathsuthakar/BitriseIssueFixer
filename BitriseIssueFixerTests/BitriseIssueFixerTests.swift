b9baabe38288ddb9fe68da14c4fa8c173ffbe3df
Ramnath Suthakar <rsuthakar@andigital.com>
2018-06-20 17:12:16 +0100

    Initial Commit

---
+++ BitriseIssueFixerTests/BitriseIssueFixerTests.swift
@@ -0,0 +1,36 @@
+//
+//  BitriseIssueFixerTests.swift
+//  BitriseIssueFixerTests
+//
+//  Created by Ramnath Suthakar on 20/06/2018.
+//  Copyright © 2018 British Airways. All rights reserved.
+//
+
+import XCTest
+@testable import BitriseIssueFixer
+
+class BitriseIssueFixerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
